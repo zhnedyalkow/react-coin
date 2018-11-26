e44904fb2b584309b9e426b1d714cadb6ed1677b
zh.nedyalkow <zh.nedyalkow@gmail.com>
2018-11-26 09:26:33 -0800

    Initial commit

---
+++ react-coin/src/components/common/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import logo from './logo.png';
+
+const Header = () => {
+    return (
+        <div className='Header'>
+            <img src={logo} alt='logo' className='Header-logo' />
+        </div>
+    );
+}
+
+export default Header;
