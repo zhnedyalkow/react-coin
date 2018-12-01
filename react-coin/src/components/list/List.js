e9ecb1f92b706a8c7af28287c055af8776ff6909
zh.nedyalkow <zh.nedyalkow@gmail.com>
2018-12-01 07:38:40 -0800

    List components, helper, config url

--- react-coin/src/components/list/List.js
+++ react-coin/src/components/list/List.js
@@ -1,52 +1,105 @@
 import React from "react";
+import { handleResponse } from "../../helpers";
+import { API_URL } from "../../config";
+import './Table.css';
+import Loading from '../common/Loading';
 
 class List extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      loading: false,
-      currencies: [],
-      error: null
-    };
-  }
+        this.state = {
+            loading: false,
+            currencies: [],
+            error: null
+        };
+    }
 
-  componentWillMount() {}
-
-  componentWillReceiveProps() {}
-  // life cycle method
+z
+    // life cycle method
     componentDidMount() {
         this.setState({ loading: true });
-        fetch("https://api.udilia.com/coins/v1/cryptocurrencies?page=1&perPage=20")
-            .then(response => {
-                return response.json().then(json => {
-                    return response.ok ? json : Promise.reject(json);
-                });
-            })
+        
+        fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
+            .then(handleResponse)
             .then(data => {
                 this.setState(
-                    { 
+                    {
                         currencies: data.currencies, loading: false
                     }
                 );
             })
             .catch(error => {
                 this.setState(
-                    { 
+                    {
                         error: error.errorMessage, loading: false
                     }
                 );
             });
     }
 
-  render() {
-      console.log(this.state)
-    if (this.state.loading) {
-      return <div> Loading... </div>;
+    renderChangePercent(percent) {
+       if (percent > 0) {
+           return <span className="percent-raised">{percent}% &uarr;</span>
+       } else if (percent < 0) {
+           return <span className="percent-fallen">{percent}% &darr;</span>
+       } else {
+           return <span>{percent}</span>
+       }
     }
 
-    return <div>Text</div>;
-  }
+    render() {
+
+        const { loading, error, currencies } = this.state;
+
+        /** Render only loading component, if loading state is set to true */
+       
+        if (loading) {
+            return <div className="loading-container"> <Loading /> </div>;
+        }
+
+         /** Render only error message, if error occured while fetching data */
+    
+        if (error) {
+            return <div className="error">{error}</div>
+        }
+
+        return (
+
+            <div className="Table-container">
+                <table className="Table">
+                    <thead className="Table-head">
+                        <tr>
+                            <th>Cryptocurrency</th>
+                            <th>Price</th>
+                            <th>Market Cap</th>
+                            <th>24H Change</th>
+                        </tr>
+                    </thead>
+                    <tbody className="Table-body">
+                        {currencies.map((currency) => {
+                            // console.log(currency)
+                            return <tr key={currency.id}>
+                                <td>
+                                    <span className="Table-rank">{currency.rank}</span>
+                                    {currency.name}
+                                </td>
+                                <td>
+                                    <span className="Table-rank">$ {currency.price}</span>
+                                </td>
+                                <td>
+                                    <span className="Table-rank">$ {currency.marketCap}</span>
+                                </td>
+                                <td>
+                                    {this.renderChangePercent(currency.percentChange24h)}
+                                </td>
+                            </tr>
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
 
 export default List;
